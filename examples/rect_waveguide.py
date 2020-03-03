29fc8437e45792a02f88de596dc0ac73050f0c30
Matt Huszagh <huszaghmatt@gmail.com>
2020-03-02 16:00:54 -0800

    remove rect_waveguide example

--- examples/rect_waveguide.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from pyems.port import standard_waveguides
-from pyems.sims.transmission_line import RectWaveguideSimulation
-
-wg = standard_waveguides["WR159"]
-sim = RectWaveguideSimulation()
-sim.rectwg_sim(5.6e9, 1e9, wg, True)
