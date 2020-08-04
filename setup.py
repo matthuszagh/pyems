3b3a28968470df090904794845757657c3092a79
Matt Huszagh <huszaghmatt@gmail.com>
2020-08-03 21:32:33 -0700

    setup.py: add information

--- setup.py
+++ setup.py
@@ -3,6 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="pyems",
     version="0.1.0",
-    # python_requires="3.7",
+    author="Matt Huszagh",
+    author_email="huszaghmatt@gmail.com",
+    description="High-level python interface to OpenEMS with automatic mesh generation",
+    license="GPL3",
     packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "scipy",
+    ],
+    url="https://github.com/matthuszagh/pyems",
 )
