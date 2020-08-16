83fc7f52ade1e16319e308baa17d49a593b136f3
Matt Huszagh <huszaghmatt@gmail.com>
2020-08-15 18:30:41 -0700

    rf_via example: adjust for newer api

--- examples/rf_via.py
+++ examples/rf_via.py
@@ -6,33 +6,33 @@ from pyems.simulation import Simulation
 from pyems.field_dump import FieldDump
 from pyems.utilities import print_table, mil_to_mm
 from pyems.structure import PCB, Microstrip, Via
-from pyems.coordinate import Box2, Coordinate2, Axis
+from pyems.coordinate import Box2, Axis
 from pyems.mesh import Mesh
 
 
 unit = 1e-3
 pcb_prop = common_pcbs["oshpark4"]
 z0_ref = 50
-freq = np.linspace(4e9, 8e9, 501)
-pcb_len = 40
+freq = np.linspace(0, 18e9, 501)
+pcb_len = 20
 pcb_width = 10
-gcpw_width = 0.34
-gcpw_gap = mil_to_mm(6)
-via_gap = 0.4
-# drill_radius = gcpw_width / 2 - pcb_prop.via_plating_thickness(unit)
+microstrip_width = 0.38
 drill_radius = 0.13
 drill_diam = 2 * drill_radius
 annular_width = 0.145
-antipad = gcpw_gap
+antipad = mil_to_mm(6)
 keepout_radius = drill_radius + annular_width + antipad
 via_sep = 0.76
-via_fence_sep = via_sep * 2
 
 sim = Simulation(freq=freq, unit=unit)
-pcb = PCB(sim=sim, pcb_prop=pcb_prop, length=pcb_len, width=pcb_width)
-box = Box2(
-    Coordinate2(-pcb_len / 2, -gcpw_width / 2), Coordinate2(0, gcpw_width / 2),
+pcb = PCB(
+    sim=sim,
+    pcb_prop=pcb_prop,
+    length=pcb_len,
+    width=pcb_width,
+    omit_copper=[0, 3],
 )
+box = Box2((-pcb_len / 2, -microstrip_width / 2), (0, microstrip_width / 2))
 Microstrip(
     pcb=pcb,
     position=box.center(),
@@ -41,17 +41,15 @@ Microstrip(
     propagation_axis=Axis("x"),
     trace_layer=0,
     gnd_layer=1,
-    gnd_gap=(gcpw_gap, gcpw_gap),
-    via_gap=(via_gap, via_gap),
-    via=None,
     port_number=1,
-    feed_shift=0.2,
+    feed_shift=0.3,
     excite=True,
+    ref_impedance=z0_ref,
 )
 
 Via(
     pcb=pcb,
-    position=Coordinate2(0, 0),
+    position=(0, 0),
     drill=drill_diam,
     annular_ring=annular_width,
     antipad=antipad,
@@ -60,7 +58,7 @@ Via(
 
 Via(
     pcb=pcb,
-    position=Coordinate2(0, -via_sep),
+    position=(0, -via_sep),
     drill=drill_diam,
     annular_ring=annular_width,
     antipad=antipad,
@@ -68,14 +66,12 @@ Via(
 
 Via(
     pcb=pcb,
-    position=Coordinate2(0, via_sep),
+    position=(0, via_sep),
     drill=drill_diam,
     annular_ring=annular_width,
     antipad=antipad,
 )
-box = Box2(
-    Coordinate2(pcb_len / 2, gcpw_width / 2), Coordinate2(0, -gcpw_width / 2),
-)
+box = Box2((pcb_len / 2, microstrip_width / 2), (0, -microstrip_width / 2))
 Microstrip(
     pcb=pcb,
     position=box.center(),
@@ -84,20 +80,17 @@ Microstrip(
     propagation_axis=Axis("x", direction=-1),
     trace_layer=3,
     gnd_layer=2,
-    gnd_gap=(gcpw_gap, gcpw_gap),
-    via_gap=(via_gap, via_gap),
-    via=None,
     port_number=2,
     excite=False,
+    ref_impedance=z0_ref,
 )
 
 mesh = Mesh(
     sim=sim,
     metal_res=1 / 80,
     nonmetal_res=1 / 40,
-    smooth=(1.3, 1.5, 1.5),
     min_lines=3,
-    expand_bounds=((0, 0), (8, 8), (8, 8)),
+    expand_bounds=((0, 0), (0, 0), (20, 20)),
 )
 
 dump = FieldDump(sim=sim, box=mesh.sim_box(include_pml=False))
@@ -108,5 +101,5 @@ sim.view_field()
 print_table(
     data=[sim.freq / 1e9, sim.s_param(1, 1), sim.s_param(2, 1)],
     col_names=["freq", "s11", "s21"],
-    prec=[4, 4, 4],
+    prec=[2, 4, 4],
 )
