5fa0ce34d024d9aef0b27caa991de8a802b945d1
Matt Huszagh <huszaghmatt@gmail.com>
2020-09-20 13:46:41 -0700

    flake.nix: add numpy, scipy and pytest dependencies

--- flake.nix
+++ flake.nix
@@ -4,24 +4,29 @@
   inputs.nixpkgs.url = github:nixos/nixpkgs/master;
   inputs.nixos.url = github:matthuszagh/nixos/master;
 
-  outputs = { self
-  , nixpkgs
-  , nixos
-  }: {
-    devShell.x86_64-linux = let
-      system = "x86_64-linux";
-      npkgs = import nixpkgs { inherit system; };
-      pkgs = nixos.packages."${system}";
-      pythonEnv = pkgs.python3.withPackages (p: with p; [
-        (pyems.overrideAttrs (old: { src = ./.; }))
-      ]);
-    in
-    npkgs.mkShell {
-      buildInputs = [
-        pythonEnv
-      ] ++ (with pkgs; [
-        paraview
-      ]);
+  outputs =
+    { self
+    , nixpkgs
+    , nixos
+    }: {
+      devShell.x86_64-linux =
+        let
+          system = "x86_64-linux";
+          npkgs = import nixpkgs { inherit system; };
+          pkgs = nixos.packages."${system}";
+          pythonEnv = pkgs.python3.withPackages (p: with p; [
+            (pyems.overrideAttrs (old: { src = ./.; }))
+            pytest
+            numpy
+            scipy
+          ]);
+        in
+        npkgs.mkShell {
+          buildInputs = [
+            pythonEnv
+          ] ++ (with pkgs; [
+            paraview
+          ]);
+        };
     };
-  };
 }
