24d7b2f3be5e003a6d1a89360aafc97627789238
Matt Huszagh <huszaghmatt@gmail.com>
2022-03-17 18:00:12 -0700

    fix test_corner

--- tests/test_coordinate.py
+++ tests/test_coordinate.py
@@ -1,18 +1,23 @@
-import pytest
+import numpy as np
 from pyems.coordinate import Box3, Coordinate3
 
 
 def test_corner():
     box = Box3(Coordinate3(0, 0, 0), Coordinate3(1, 1, 1))
     corners = box.corners()
-    corners = [corner.coordinate_list() for corner in corners]
-    assert corners == [
-        Coordinate3(0, 0, 0).coordinate_list(),
-        Coordinate3(0, 1, 0).coordinate_list(),
-        Coordinate3(1, 0, 0).coordinate_list(),
-        Coordinate3(1, 1, 0).coordinate_list(),
-        Coordinate3(0, 0, 1).coordinate_list(),
-        Coordinate3(0, 1, 1).coordinate_list(),
-        Coordinate3(1, 0, 1).coordinate_list(),
-        Coordinate3(1, 1, 1).coordinate_list(),
-    ]
+    corners = np.array([corner.coordinate_list() for corner in corners])
+    assert np.array_equal(
+        corners,
+        np.array(
+            [
+                Coordinate3(0, 0, 0).coordinate_list(),
+                Coordinate3(0, 1, 0).coordinate_list(),
+                Coordinate3(1, 0, 0).coordinate_list(),
+                Coordinate3(1, 1, 0).coordinate_list(),
+                Coordinate3(0, 0, 1).coordinate_list(),
+                Coordinate3(0, 1, 1).coordinate_list(),
+                Coordinate3(1, 0, 1).coordinate_list(),
+                Coordinate3(1, 1, 1).coordinate_list(),
+            ]
+        ),
+    )
